04fa27cb0a949ae65718a416108ff056267c0f70
Rafiu Moshood <meezyloaded@gmail.com>
2021-10-29 19:18:31 +0100

    fix batch payouts

--- app/Imports/BatchPayoutImport.php
+++ app/Imports/BatchPayoutImport.php
@@ -15,17 +15,21 @@ class BatchPayoutImport implements ToModel, WithCalculatedFormulas
     */
     public function model(array $row)
     {
-        return new BatchPayout([
-            'batch' => $row[0] ?? null,
-            'name' => $row[1] ?? null,
-            'email' => $row[2] ?? null,
-            'phone' => $row[3] ?? null,
-            'units' => $row[4] ?? null,
-            'amount_invested' => $row[5] ?? null,
-            'expected_returns' => $row[6] ?? null,
-            'farm_cycle' => $row[7] ?? null,
-            'payment_date' => $row[8] ? \PhpOffice\PhpSpreadsheet\Shared\Date::excelToDateTimeObject((float)$row[8]) : null,
-            'queue' => $row[9] ?? null,
-        ]);
+        $allNull = true;
+        for ($i = 0; $i < count($row); $i++) if ($row[$i]) $allNull = false;
+        if (!$allNull) {
+            return new BatchPayout([
+                'batch' => $row[0] ?? null,
+                'name' => $row[1] ?? null,
+                'email' => $row[2] ?? null,
+                'phone' => $row[3] ?? null,
+                'units' => $row[4] ?? null,
+                'amount_invested' => $row[5] ?? null,
+                'expected_returns' => $row[6] ?? null,
+                'farm_cycle' => $row[7] ?? null,
+                'payment_date' => $row[8] ? \PhpOffice\PhpSpreadsheet\Shared\Date::excelToDateTimeObject((float)$row[8]) : null,
+                'queue' => $row[9] ?? null,
+            ]);
+        }
     }
 }
